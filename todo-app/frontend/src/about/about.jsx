d039d7dcbb2968e5a02b55153a563622cee31d4b
duartels <40619503+duartels@users.noreply.github.com>
2020-04-27 17:54:23 -0300

    .

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa história</h2>
+        <p>Naisdjoaskd asdjasodj asdjoas...</p>
+        <h2>Missão e Visão</h2>
+        <p>SDFnmsodkf ijsdij isdjiod</p>
+        <h2>Imprensa</h2>
+        <p>sdfijsd sdfkjosd sdokfd</p>
+    </div>
+)

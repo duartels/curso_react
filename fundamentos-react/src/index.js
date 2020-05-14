da6c33a8c1dfd86764cc93620057b11f2e1ec6e1
duartels <40619503+duartels@users.noreply.github.com>
2020-05-14 20:31:16 -0300

    .

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const el = document.getElementById('root')
+ReactDOM.render(
+    <div>
+        <strong>Hello React</strong>
+    </div>, 
+    el)

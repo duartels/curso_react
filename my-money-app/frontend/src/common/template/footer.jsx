daee5a2072f181555f7d3f6a7b54f0a3eacbdeeb
duartels <40619503+duartels@users.noreply.github.com>
2020-05-26 11:53:08 -0300

    .

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) => {
+    return(
+        <footer className="main-footer">
+            <strong>
+                Copyright &copy; 2020
+                <a href="http://emsoft.com.br" target='blank'> emsoft</a>
+            </strong>
+        </footer>
+    )
+    
+}
